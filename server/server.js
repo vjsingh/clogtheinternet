39bf85512d9c211a27e5d7170de81d3b00e2b172
Jonathan Koh <jonathanvkoh@gmail.com>
2020-06-05 17:06:26 -0400

    Hashtag generator update

--- server/server.js
+++ server/server.js
@@ -19,14 +19,41 @@ app.get('/get_hashtags', (req, res) => {
   const randomHashtags = getHashtagList('random.csv');
   const altRightHashtags = getHashtagList('altright.csv');
   const popularHashtags = getHashtagList('popular.csv');
-  res.send({
-    hashtags: _.concat(randomHashtags, altRightHashtags, popularHashtags),
-  });
+  console.log(randomHashtags, altRightHashtags, popularHashtags);
+
+  const outputHashtags = [];
+  const numRandom = 15;
+  for (let i = 0; i < numRandom; i++) { addOutputHashtag(outputHashtags, randomHashtags); }
+
+  const numAltRight = 13;
+  for (let i = 0; i < numAltRight; i++) { addOutputHashtag(outputHashtags, altRightHashtags); }
+
+  const numPopular = 2;
+  for (let i = 0; i < numPopular; i++) { addOutputHashtag(outputHashtags, popularHashtags); }
+
+  res.send({ hashtags: outputHashtags });
 });
+
+function addOutputHashtag(outputHashtags, list) {
+  let tries = 20;
+  let newHashtag = getRandomFromList(list);
+  while (_.includes(outputHashtags, newHashtag) && tries > 0) {
+    newHashtag = getRandomFromList(list);
+    console.log("NEW HASHTAG", newHashtag);
+    console.log("TRY #", tries);
+    tries--;
+  }
+  outputHashtags.push(newHashtag);
+}
 
 function getHashtagList(filename) {
   const filePath = `hashtags/${filename}`;
   const fData = fs.readFileSync(filePath).toString();
   const filtered_hashtags = _.filter(_.split(fData, '\n'), (val) => val.length > 0);
   return _.map(filtered_hashtags, (str) => `#${str}`);
+}
+
+function getRandomFromList(list) {
+  const randomIndex = Math.floor(Math.random()*list.length);
+  return list[randomIndex];
 }

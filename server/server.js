e65128825784f94012165447c74f27925288ceca
vjsingh <varun101@gmail.com>
2020-06-05 16:11:45 -0400

    Enable server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, '../build')));
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 // create a GET route

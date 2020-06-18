0873188994a478f5e678a2f9911a1eed07cb4d95
Jonathan Koh <jonathanvkoh@gmail.com>
2020-06-17 22:12:40 -0400

    Require https before static assets

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../build')));
 app.use(requireHTTPS);
+app.use(express.static(path.join(__dirname, '../build')));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
